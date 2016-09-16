a7e85fc7e21bf47d2832b86aa31cc7c619eb6928
Doug DeWan <Doug DeWan@DESKTOP-0KOCPR3>
2016-09-15 21:48:46 -0400

    Login Test

--- src/com/dsdewan/oppmjavautility/OPPMJavaUtility.java
+++ src/com/dsdewan/oppmjavautility/OPPMJavaUtility.java
@@ -5,6 +5,9 @@
  */
 package com.dsdewan.oppmjavautility;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Doug DeWan
@@ -16,6 +19,23 @@ public class OPPMJavaUtility {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        OPPMJavaUtilityEnvironment oppmWSEnvs = new OPPMJavaUtilityEnvironment();
+        
+        oppmWSEnvs.setUserName("admin");
+        oppmWSEnvs.setPassword("");
+        oppmWSEnvs.setTimeOut(600);
+        oppmWSEnvs.setServer("http://oppm");
+        oppmWSEnvs.setWebServiceLocation("/prosightWS");
+        
+        OPPMWSClient oWSClient = new OPPMWSClient();
+        
+        try {
+            System.out.println("Login Successful: " + oWSClient.Login(oppmWSEnvs));
+            System.out.println("Login Cookie: " + oppmWSEnvs.getLoginCookie());
+        } catch (Exception ex) {
+            Logger.getLogger(OPPMJavaUtility.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
