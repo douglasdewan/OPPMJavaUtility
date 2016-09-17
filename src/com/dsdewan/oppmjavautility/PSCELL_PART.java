c5b6f5ee11acad1f9b904be71295521445b516b1
Doug DeWan <Doug DeWan@DESKTOP-0KOCPR3>
2016-09-17 09:15:36 -0400

    Initial Creation

---
+++ src/com/dsdewan/oppmjavautility/PSCELL_PART.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dsdewan.oppmjavautility;
+
+/**
+ *
+ * @author Doug DeWan
+ */
+public enum PSCELL_PART {
+    CPRT_NONE(-2), 
+    CPRT_VALUE(0),
+    CPRT_INDICATOR_COLOR(2);
+    
+    private Integer iCellPartValue;
+    
+    private PSCELL_PART(Integer iCellPartValueIn){
+        this.iCellPartValue = iCellPartValueIn;
+    }
+}
